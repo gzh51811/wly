6ceef2f8cd19d99b9dff94a08dc2e3a5b51a640e
YKB2333 <46928117+YKB2333@users.noreply.github.com>
2019-03-01 11:33:57 +0800

    Update server_ysb.js

--- src/server_ysb.js
+++ src/server_ysb.js
@@ -2,19 +2,13 @@ const Koa = require('koa');
 const static = require('koa-static');
 
 // 路由
-<<<<<<< HEAD
+
 const routers = require('./js/index');
-=======
-const routers = require('./api/routers');
->>>>>>> 9e2f493f45c26dbf44bf1257098390a8bb4616cd
+
 
 // 创建koa应用
 const app = new Koa();//app.context
 
-<<<<<<< HEAD
-app.context.myname = 'laoxie'
-=======
->>>>>>> 9e2f493f45c26dbf44bf1257098390a8bb4616cd
 
 // 创建静态资源服务
 app.use(static('./'));
@@ -23,10 +17,8 @@ app.use(routers.allowedMethods());
 app.use(routers.routes());
 
 // 监听端口
-<<<<<<< HEAD
-app.listen(1811, () => {
-=======
+
 app.listen(1811,()=>{
->>>>>>> 9e2f493f45c26dbf44bf1257098390a8bb4616cd
+
     console.log('server is running on http://localhost:1811');
 })
